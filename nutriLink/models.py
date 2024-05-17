4274be8f28a7a5836f2f0d7930bcef0b637931bd
mmkkozak <kmichalina000@gmail.com>
2024-05-17 21:39:08 +0200

    database tables created
    first migration - database tables for the website were created

--- nutriLink/models.py
+++ nutriLink/models.py
@@ -1,3 +1,43 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    email = models.EmailField(max_length=50, unique=True)
+    password = models.CharField(max_length=20)
+
+class Recipe(models.Model):
+    recipe_name = models.CharField(max_length=150, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    contents = models.TextField()
+    picture = models.ImageField()
+    pub_date = models.DateTimeField()
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    kcal = models.IntegerField()
+    amount = models.IntegerField()
+    unit = models.CharField(max_length=20)
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    comment = models.TextField()
+    pub_date = models.DateTimeField()
+
+class RecipeIngredient(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    other_measure = models.CharField(max_length=100, blank=True, null=True)
+
+class Diet(models.Model):
+    diet_name = models.CharField(max_length=100, unique=True)
+
+class Exclusion(models.Model):
+    diet = models.ForeignKey(Diet, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    max_amount = models.IntegerField()
+
